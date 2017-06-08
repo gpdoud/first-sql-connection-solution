fb3f020c92e352eee5874fa44b8ab42dedabf961
gpdoud <gpdoud@gmail.com>
2017-06-08 15:13:24 -0400

    Implemented static SELECT and static UPDATE in User class

--- FirstSqlConnectionProject/Program.cs
+++ FirstSqlConnectionProject/Program.cs
@@ -5,51 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using PrsLibrary;
 
 namespace FirstSqlConnectionProject {
     class Program {
         static void Main(string[] args) {
 
-            string ConnStr = @"Server=DSI-WORKSTATION\SQLEXPRESS;Database=prs;Trusted_Connection=True;";
-            SqlConnection Conn = new SqlConnection(ConnStr);
-            Conn.Open();
-            if(Conn.State != ConnectionState.Open) {
-                throw new ApplicationException("Connection didn't open");
-            }
+            //            string ConnStr = @"Server=DSI-WORKSTATION\SQLEXPRESS;Database=prs;Trusted_Connection=True;";
+            //            SqlConnection Conn = new SqlConnection(ConnStr);
+            //            Conn.Open();
+            //            if(Conn.State != ConnectionState.Open) {
+            //                throw new ApplicationException("Connection didn't open");
+            //            }
 
-            // Insert
-            string SqlInsert = @"insert [user] (UserName, Password, FirstName, LastName, Phone, Email, IsAdmin, IsReviewer)
-values(@username, 'password', 'Greg', 'Doud', '513-703-7315', 'gpdoud@gmail.com', @isadmin, 1)";
-            SqlCommand CmdInsert = new SqlCommand(SqlInsert, Conn);
-            CmdInsert.Parameters.Add(new SqlParameter("@username", "gpdoud"));
-            CmdInsert.Parameters.Add(new SqlParameter("@isadmin", "1"));
-            int recsAffected = CmdInsert.ExecuteNonQuery();
-            if(recsAffected != 1) {
-                throw new ApplicationException("Insert failed!");
-            }
+            //            // Insert
+            //            string SqlInsert = @"insert [user] (UserName, Password, FirstName, LastName, Phone, Email, IsAdmin, IsReviewer)
+            //values(@username, 'password', 'Greg', 'Doud', '513-703-7315', 'gpdoud@gmail.com', @isadmin, 1)";
+            //            SqlCommand CmdInsert = new SqlCommand(SqlInsert, Conn);
+            //            CmdInsert.Parameters.Add(new SqlParameter("@username", "gpdoud"));
+            //            CmdInsert.Parameters.Add(new SqlParameter("@isadmin", "1"));
+            //            int recsAffected = CmdInsert.ExecuteNonQuery();
+            //            if(recsAffected != 1) {
+            //                throw new ApplicationException("Insert failed!");
+            //            }
 
             // Select
-            string Sql = "Select * from [user]";
-            SqlCommand Cmd = new SqlCommand(Sql, Conn);
-            SqlDataReader Reader = Cmd.ExecuteReader();
-            if(!Reader.HasRows) {
-                throw new ApplicationException("Result set has no rows!");
-            }
-            while(Reader.Read()) {
-                int id = Reader.GetInt32(Reader.GetOrdinal("Id"));
-                string userName = Reader.GetString(Reader.GetOrdinal("UserName"));
-                string firstName = Reader.GetString(Reader.GetOrdinal("FirstName"));
-                string lastName = Reader.GetString(Reader.GetOrdinal("LastName"));
-                string phone = Reader.GetString(Reader.GetOrdinal("Phone"));
-                string email = Reader.GetString(Reader.GetOrdinal("Email"));
-                bool isReviewer = Reader.GetBoolean(Reader.GetOrdinal("IsReviewer"));
-                bool isAdmin = Reader.GetBoolean(Reader.GetOrdinal("IsAdmin"));
+            string whereClause = "Id = 7";
+            string orderByClause = "UserName desc";
+            UserCollection users = User.Select(whereClause, orderByClause);
+            User user = users[0];
+            user.FirstName = "Greg";
+            bool success = User.Update(user);
 
-                string message = string.Format("{0} - {1} {2}", id, firstName, lastName);
-                System.Diagnostics.Debug.WriteLine(message);
-            }
-
-            Conn.Close();
+            int i = 0;
         }
     }
 }
---
+++ PrsLibrary/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PrsLibrary {
+
+    public class User {
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsReviewer { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public static bool Update(User user) {
+            string Sql = string.Format("UPDATE [user] Set " +
+                    " UserName = @username, " +
+                    " Password = @password, " +
+                    " FirstName = @firstname, " +
+                    " LastName = @lastname, " +
+                    " Phone = @phone, " +
+                    " Email = @email, " +
+                    " IsReviewer = @isreviewer, " +
+                    " IsAdmin = @isadmin " +
+                    " WHERE ID = @Id; ");
+            string ConnStr = @"Server=DSI-WORKSTATION\SQLEXPRESS;Database=prs;Trusted_Connection=True;";
+            SqlConnection Conn = new SqlConnection(ConnStr);
+            Conn.Open();
+            if (Conn.State != ConnectionState.Open) {
+                throw new ApplicationException("Connection didn't open");
+            }
+            SqlCommand Cmd = new SqlCommand(Sql, Conn);
+            Cmd.Parameters.Add(new SqlParameter("@id", user.Id));
+
+            Cmd.Parameters.Add(new SqlParameter("@username", user.UserName));
+            Cmd.Parameters.Add(new SqlParameter("@password", user.Password));
+            Cmd.Parameters.Add(new SqlParameter("@firstname", user.FirstName));
+            Cmd.Parameters.Add(new SqlParameter("@lastname", user.LastName));
+            Cmd.Parameters.Add(new SqlParameter("@phone", user.Phone));
+            Cmd.Parameters.Add(new SqlParameter("@email", user.Email));
+            Cmd.Parameters.Add(new SqlParameter("@isreviewer", user.IsReviewer));
+            Cmd.Parameters.Add(new SqlParameter("@isadmin", user.IsAdmin));
+            int recsAffected = Cmd.ExecuteNonQuery();
+            if(recsAffected != 1) {
+                throw new ApplicationException("Update Failed!");
+            }
+            return (recsAffected == 1);
+        }
+
+        public static UserCollection Select(string whereClause, string orderByClause) {
+            string Sql = string.Format("SELECT * from [User] WHERE ({0}) ORDER BY {1}", whereClause, orderByClause);
+            string ConnStr = @"Server=DSI-WORKSTATION\SQLEXPRESS;Database=prs;Trusted_Connection=True;";
+            SqlConnection Conn = new SqlConnection(ConnStr);
+            Conn.Open();
+            if (Conn.State != ConnectionState.Open) {
+                throw new ApplicationException("Connection didn't open");
+            }
+            SqlCommand Cmd = new SqlCommand(Sql, Conn);
+            SqlDataReader Reader = Cmd.ExecuteReader();
+            if (!Reader.HasRows) {
+                throw new ApplicationException("Result set has no rows!");
+            }
+            UserCollection users = new UserCollection();
+            while (Reader.Read()) {
+                int id = Reader.GetInt32(Reader.GetOrdinal("Id"));
+                string userName = Reader.GetString(Reader.GetOrdinal("UserName"));
+                string password = Reader.GetString(Reader.GetOrdinal("Password"));
+                string firstName = Reader.GetString(Reader.GetOrdinal("FirstName"));
+                string lastName = Reader.GetString(Reader.GetOrdinal("LastName"));
+                string phone = Reader.GetString(Reader.GetOrdinal("Phone"));
+                string email = Reader.GetString(Reader.GetOrdinal("Email"));
+                bool isReviewer = Reader.GetBoolean(Reader.GetOrdinal("IsReviewer"));
+                bool isAdmin = Reader.GetBoolean(Reader.GetOrdinal("IsAdmin"));
+
+                User user = new User();
+                user.Id = id;
+                user.UserName = userName;
+                user.Password = password;
+                user.FirstName = firstName;
+                user.LastName = lastName;
+                user.Email = email;
+                user.Phone = phone;
+                user.IsReviewer = isReviewer;
+                user.IsAdmin = isAdmin;
+
+                users.Add(user);
+            }
+
+            Conn.Close();
+            return users;
+        }
+    }
+}
